0bf81202b46b27e4b2bc0b8152dd759299720fbf
GeVhoo <gvahrushev@mail.ru>
2019-12-11 16:10:51 +0500

    Step1

--- diff_calc/gendiff.py
+++ diff_calc/gendiff.py
@@ -1,3 +1,14 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str, help='input name')
+parser.add_argument('second_file', type=str, help='input name')
+parser.add_argument('-f', '--format', type=str, help='set format of output')
+args = parser.parse_args()
+print(args.indir)
+
+
 def main():
     print('Hello World!')
 
